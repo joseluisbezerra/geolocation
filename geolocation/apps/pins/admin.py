db83fa0091d016826fbcc40f2eba7f55be51e652
Luis Bezerra <luisbezerra.work@gmail.com>
2022-06-11 11:50:30 -0300

    Criação do modelo Pin, adição da extenssão do postgis na migration

--- geolocation/apps/pins/admin.py
+++ geolocation/apps/pins/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 
-# Register your models here.
+from .models import Pin
+
+
+@admin.register(Pin)
+class PinAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
--- geolocation/apps/pins/models.py
+++ geolocation/apps/pins/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Pin(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.PointField()
+    expiration_date = models.DateField()
+
+    class Meta:
+        db_table = 'pin'
+
+    def __str__(self) -> str:
+        return self.name
