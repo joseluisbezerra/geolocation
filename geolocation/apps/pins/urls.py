b0ac23d4cd8bfc1ef0c4a63275759e19c4fcb7d7
Luis Bezerra <luisbezerra.work@gmail.com>
2022-06-11 15:42:55 -0300

    Plotagem dos pins no mapa

--- geolocation/apps/pins/urls.py
+++ geolocation/apps/pins/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from geolocation.apps.pins import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('pins/', views.pins_list, name='pins_list')
 ]
--- geolocation/apps/pins/views.py
+++ geolocation/apps/pins/views.py
@@ -1,5 +1,26 @@
+from datetime import date
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.gis.db.models.functions import AsGeoJSON
+
+from geolocation.apps.pins.models import Pin
 
 
 def home(request):
     return render(request, 'pins/home.html')
+
+
+def pins_list(request):
+    pins = list(
+        Pin.objects.filter(
+            expiration_date__gte=date.today()
+        ).annotate(
+            pin_location=AsGeoJSON('location')
+        ).values(
+            'name',
+            'pin_location'
+        )
+    )
+
+    return JsonResponse(pins, safe=False)
