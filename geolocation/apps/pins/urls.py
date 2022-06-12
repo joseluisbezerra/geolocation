acc73dd72abaa736dd9b82d01d5139694fed8966
Luis Bezerra <luisbezerra.work@gmail.com>
2022-06-12 20:06:22 -0300

    Criação da funcionalidade de apagar um pin

--- geolocation/apps/pins/urls.py
+++ geolocation/apps/pins/urls.py
@@ -4,5 +4,8 @@ from geolocation.apps.pins import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('pins/', views.pins_list, name='pins_list')
+
+    path('pins/', views.pins_list, name='pins_list'),
+
+    path('delete/<int:id>/', views.delete_pin, name='delete_pin')
 ]
--- geolocation/apps/pins/views.py
+++ geolocation/apps/pins/views.py
@@ -1,8 +1,13 @@
 from datetime import date
 
-from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.gis.db.models.functions import AsGeoJSON
+from django.http import JsonResponse
+from django.contrib import messages
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect
+)
 
 from geolocation.apps.pins.models import Pin
 
@@ -18,9 +23,21 @@ def pins_list(request):
         ).annotate(
             pin_location=AsGeoJSON('location')
         ).values(
+            'id',
             'name',
+            'expiration_date',
             'pin_location'
         )
     )
 
     return JsonResponse(pins, safe=False)
+
+
+def delete_pin(request, id):
+    pin = get_object_or_404(Pin, pk=id)
+
+    pin.delete()
+
+    messages.success(request, 'Successfully deleted pin')
+
+    return redirect('/')
